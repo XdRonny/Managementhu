28c433472bf101c16f2fbf2d6fa866f6661c292b
UNKNOWN <79321850+Jatindalal875@users.noreply.github.com>
2023-07-14 21:10:21 +0530

    Update __main__.py

--- MukeshRobot/__main__.py
+++ MukeshRobot/__main__.py
@@ -202,8 +202,7 @@ def start(update: Update, context: CallbackContext):
             first_name = update.effective_user.first_name
             
             x=update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAI33mLYLNLilbRI-sKAAob0P7koTEJNAAIOBAACl42QVKnra4sdzC_uKQQ")
-            x.delete()
+                "CAACAgUAAx0CdVhmXwABAiEhZLFrRD9Di-t9XfNWVkuQpuudaEgAAtsJAALOHZBVntJm5ykwboEvBA")
             usr = update.effective_user
             lol = update.effective_message.reply_text(
                 PM_START_TEX.format(usr.first_name), parse_mode=ParseMode.MARKDOWN
